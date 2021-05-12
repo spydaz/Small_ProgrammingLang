2e3e1d4691ff45339f2a49d37e8bae9a17cf1f74
Leroy Dyer <leroysamueldyer@hotmail.co.uk>
2021-05-12 19:29:22 +0100

    cleanup

--- LANGS/S_ExpressionTests/math-test.js
+++
@@ -1,7 +0,0 @@
-const assert = require('assert');
-
-module.exports = eva => {
-  assert.strictEqual(eva.eval(['+', 1, 5]), 6);
-  assert.strictEqual(eva.eval(['+', ['+', 3, 2], 5]), 10);
-  assert.strictEqual(eva.eval(['+', ['*', 3, 2], 5]), 11);
-};
--- LANGS/S_ExpressionTests/self-eval-test.js
+++
@@ -1,6 +0,0 @@
-const assert = require('assert');
-
-module.exports = eva => {
-  assert.strictEqual(eva.eval(1), 1);
-  assert.strictEqual(eva.eval('"hello"'), 'hello');
-};
